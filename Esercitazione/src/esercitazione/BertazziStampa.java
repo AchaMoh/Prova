d9c92bea37eec925d8f581c9843522df1204a454
berta <berta@LAPTOP-4P6RLPHK>
2025-04-01 13:05:56 +0200

    prima stampa

---
+++ Esercitazione/src/esercitazione/BertazziStampa.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package esercitazione;
+
+/**
+ *
+ * @author berta
+ */
+public class BertazziStampa {
+    public void stampa(){
+        System.out.println("ciao");
+    }
+}
