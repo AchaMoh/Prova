598bd2acec8ece63ff78931ca238d9061d7ddb9b
User <User@DESKTOP-5LKI89G>
2025-04-01 13:14:11 +0200

    Lippolis

---
+++ Esercitazione/src/esercitazione/Edolippo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package esercitazione;
+
+/**
+ *
+ * @author User
+ */
+public class Edolippo {
+    
+    public void stampa(){
+        System.out.println("veronica zitta!!");
+    }
+}
